1d462c49ffb5d2e6d2e28498b11c247abf3dc2d7
cyrilsofdevpro <cyrilsofdevpro@users.noreply.github.com>
2026-07-15 08:34:29 -0700

    Use dist output directory for Vercel

--- scripts/build-static.js
+++ scripts/build-static.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const srcDir = path.join(__dirname, '..');
-const outDir = path.join(__dirname, '..', 'out');
+const distDir = path.join(__dirname, '..', 'dist');
 
 function copyDir(src, dest) {
   if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
@@ -10,7 +10,7 @@ function copyDir(src, dest) {
     const destPath = path.join(dest, name);
     const stat = fs.statSync(srcPath);
     if (stat.isDirectory()) {
-      if (['node_modules', '.git', 'out', 'scripts'].includes(name)) continue;
+      if (['node_modules', '.git', 'dist', 'scripts'].includes(name)) continue;
       copyDir(srcPath, destPath);
     } else if (!['package-lock.json', 'yarn.lock', 'pnpm-lock.yaml'].includes(name)) {
       fs.copyFileSync(srcPath, destPath);
@@ -18,8 +18,8 @@ function copyDir(src, dest) {
   }
 }
 
-if (fs.existsSync(outDir)) {
-  fs.rmSync(outDir, { recursive: true, force: true });
+if (fs.existsSync(distDir)) {
+  fs.rmSync(distDir, { recursive: true, force: true });
 }
-copyDir(srcDir, outDir);
-console.log('Static site copied to out/');
+copyDir(srcDir, distDir);
+console.log('Static site copied to dist/');
